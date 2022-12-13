38c1efff8354f320d5df0736f7113fc573bdc775
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-13 19:28:44 +0000

    Finished styling of scores component

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,12 @@
-const Score = () => {
+const Score = ({ currentScore }) => {
   return (
-    <div className="flex justify-around">
-      <div className="rounded bg-red-500">Current Score: 0</div>
-      <div className="rounded bg-red-500">Best Score: 0</div>
+    <div className="flex justify-center gap-32 w-[60rem]">
+      <div className="rounded-3xl bg-rose-300 font-semibold p-2 w-1/4 text-xl text-center shadow-lg">
+        Current Score: {currentScore}
+      </div>
+      <div className="rounded-3xl bg-emerald-300 font-semibold p-2 w-1/4 text-xl text-center shadow-lg">
+        Best Score: 0
+      </div>
     </div>
   );
 };
