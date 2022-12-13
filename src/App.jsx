ece8baf8c872a9e4e11a759347948cff3da5cbcc
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-13 15:10:57 +0000

    Added foundational components to master branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
