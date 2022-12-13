2d34ede6f3a9d33266ee56f9a6038f113f1af0bc
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-13 18:12:42 +0000

    Styled score section

---
+++ src/components/Score.jsx
@@ -0,0 +1,10 @@
+const Score = () => {
+  return (
+    <div className="flex justify-around">
+      <div className="rounded bg-red-500">Current Score: 0</div>
+      <div className="rounded bg-red-500">Best Score: 0</div>
+    </div>
+  );
+};
+
+export default Score;
