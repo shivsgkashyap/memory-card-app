8ac0ea8cf7e8800a382d00d3b197632237f811bd
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-13 17:41:37 +0000

    Added logo and pokeball styling for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="flex justify-center bg-[#ee1515]">
+        <img className="max-w-2xl" src={logo} alt="pokemon logo" />
+      </header>
+      <div className="flex justify-center items-center h-5 bg-neutral-700">
+        <div className="flex justify-center items-center h-14 w-14 border-4 border-neutral-700 rounded-[50%] bg-zinc-100">
+          <div className="flex justify-center items-center h-10 w-10 border-4 border-neutral-700 rounded-[50%] bg-zinc-100"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
