69a0178be6f843774371d284cdc93dd8ccdc21ea
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-13 15:18:20 +0000

    Added personalized footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#303030] text-zinc-100 flex justify-center items-center w-full p-2">
+      <p>Copyright © 2022 Shiv Kashyap</p>
+      <a
+        href="https://github.com/shivsgkashyap"
+        target="_blank"
+        className="ml-2 text-xl text-zinc-100 hover:text-zinc-300"
+      >
+        <FaGithub />
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
